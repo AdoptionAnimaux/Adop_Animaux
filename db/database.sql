298dc2c249218388f3665c16e46dfaebae2f4597
fifii23 <poliigonfer2022@gmail.com>
2025-11-24 15:45:48 +0100

    create database

---
+++ db/database.sql
@@ -0,0 +1,74 @@
+-- =============================================
+--    Création de la base de données
+--    Application d’adoption d’animaux
+-- =============================================
+
+CREATE DATABASE IF NOT EXISTS adoption_animals;
+USE adoption_animals;
+
+-- =============================================
+-- Table person (classe mère User / Admin)
+-- =============================================
+
+CREATE TABLE person (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    phone VARCHAR(20),
+    
+    -- Pour représenter l’héritage User/Admin
+    role ENUM('user', 'admin') NOT NULL
+);
+
+-- =============================================
+-- Table animal
+-- =============================================
+
+CREATE TABLE animal (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    age INT NOT NULL,
+    category VARCHAR(50) NOT NULL,
+    description TEXT,
+    image VARCHAR(255)
+);
+
+-- =============================================
+-- Table publication_request (User -> Admin)
+-- Un user demande publication d'un animal
+-- =============================================
+
+CREATE TABLE publication_request (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    animalInfo TEXT NOT NULL,
+    status ENUM('pending', 'approved', 'rejected') DEFAULT 'pending',
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    
+    CONSTRAINT fk_pub_user 
+        FOREIGN KEY (user_id) REFERENCES person(id)
+        ON DELETE CASCADE
+);
+
+-- =============================================
+-- Table adoption_request
+-- Un user demande adopter un animal
+-- =============================================
+
+CREATE TABLE adoption_request (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    animal_id INT NOT NULL,
+    user_id INT NOT NULL,
+    date_request DATETIME DEFAULT CURRENT_TIMESTAMP,
+    status ENUM('pending', 'approved', 'rejected') DEFAULT 'pending',
+    appointment_date DATETIME,
+    
+    CONSTRAINT fk_adopt_animal
+        FOREIGN KEY (animal_id) REFERENCES animal(id)
+        ON DELETE CASCADE,
+
+    CONSTRAINT fk_adopt_user
+        FOREIGN KEY (user_id) REFERENCES person(id)
+        ON DELETE CASCADE
+);
