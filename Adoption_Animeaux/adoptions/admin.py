7d55f3e3f302d234b3037860c01d552cd06432e5
fifii23 <poliigonfer2022@gmail.com>
2025-12-04 13:59:43 +0100

    fgfg

--- Adoption_Animeaux/adoptions/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from django.contrib import admin
-from .models import AdoptionRequest
-admin.site.register(AdoptionRequest)
-
-# Register your models here.
--- Adoption_Animeaux/animals/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Animal
-
-@admin.register(Animal)
-class AnimalAdmin(admin.ModelAdmin):
-    list_display = ("name", "category", "race", "age", "available")
-    search_fields = ("name", "race", "category")
-    list_filter = ("available", "category")
