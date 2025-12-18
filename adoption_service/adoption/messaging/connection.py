5ee7479718026d2a9dcc82e9224dab424df1fa6a
Nour004 <houdamedj06@gmail.com>
2025-12-18 11:08:38 +0100

    rabbit

--- adoption_service/adoption/messaging/connection.py
+++ adoption_service/adoption/messaging/connection.py
@@ -2,9 +2,12 @@ import os
 import pika
 
 def get_connection():
+    credentials = pika.PlainCredentials("micro", "micro")
     params = pika.ConnectionParameters(
         host = os.environ.get("RABBITMQ_HOST", "localhost"),
         port=5672,
+        virtual_host="/",
+        credentials=credentials,
         socket_timeout=5
     )
     connection = pika.BlockingConnection(params)
--- notifications_service/notifications/messaging/connection.py
+++ notifications_service/notifications/messaging/connection.py
@@ -5,12 +5,14 @@ def get_connection():
     host = os.environ.get("RABBITMQ_HOST", "127.0.0.1")
     port = 5672  # Port exposé par ton container Docker
 
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(
-            host=host,
-            port=port
-        )
+    credentials = pika.PlainCredentials("micro", "micro")
+    parameters = pika.ConnectionParameters(
+        host=host,
+        port=port,
+        virtual_host="/",
+        credentials=credentials
     )
+    connection = pika.BlockingConnection(parameters)
 
     channel = connection.channel()
     return connection, channel
